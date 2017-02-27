d80e1b0a421f2d29721dce74c22ebca80b6aab66
safii <muhamadsavii@gmail.com>
2017-02-27 16:30:11 +0700

    first commit

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,50 @@
+class ResumesController < ApplicationController
+  def index
+      @resumes = Resume.all
+   end
+   
+   def new
+      @resume = Resume.new
+   end
+   
+   def create
+      @resume = Resume.new(resume_params)
+      
+      if @resume.save
+         redirect_to resumes_path, notice: "The resume #{@resume.name} has been uploaded."
+      else
+         render "new"
+      end
+      
+   end
+
+   def edit
+    @resume = Resume.find_by_id(params[:id])
+
+  end
+   
+   def destroy
+      @resume = Resume.find(params[:id])
+      @resume.destroy
+      redirect_to resumes_path, notice:  "The resume #{@resume.name} has been deleted."
+   end
+
+   def update
+
+ @resume = Resume.find_by_id(params[:id])
+
+ if @resume.update(resume_params)
+
+    flash[:notice] = "Success Update Records"
+
+    redirect_to action: 'index'
+
+ end
+
+end
+   
+   private
+      def resume_params
+      params.require(:resume).permit(:name, :attachment)
+   end
+end
